036bac713f34717e23a5ddd1c803102ff1db36e4
flaamjab <flaamjab@gmail.com>
2021-04-03 22:08:53 +0500

    add wasm runner script

---
+++ scripts/wun.ts
@@ -0,0 +1,22 @@
+function run() {
+    const code = Deno.readFileSync("tests/data/output/program.wasm");
+    
+    type programFun = () => void;
+    
+    WebAssembly
+        .instantiate(code, {
+            imports: {
+                writeln_int: writeln,
+                writeln_real: writeln
+            }
+        }).then(r => {
+            const program = r.instance.exports.program as programFun;
+            program();
+        });
+}
+
+function writeln(num: number) {
+    console.log(num);
+}
+
+run();
